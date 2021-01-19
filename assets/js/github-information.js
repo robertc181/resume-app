14c27a7bb4b4d0f0d6863b4294873f9b4e76d836
robertc181 <rdancole@gmail.com>
2021-01-19 16:31:39 +0000

    added search function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,17 +1,71 @@
+
+
+// function userInformationHTML(user) {
+//     return `
+//         <h2>${user.name}
+//             <span class="small-name">
+//                 (@<a href="${user.html_url}" target="_blank">${user-login}</a>
+//             </span>
+//         </h2> 
+//         <div class="gh-content">
+//             <div class="gh-avatar">
+//                 <a href="${user.html_url} target="_blank">
+//                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+//                 </a>
+//             </div>
+//             <p> Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}
+//             </p>
+
+//         </div>`;
+// }
+
+
 // function fetchGitHubInformation(event) {
 
 //     var username = $("#gh-username").val();
 //     if (!username) {
-//         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`)
+//         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
 //         return;
 //     }
-    
-//     $("#gh-user-data").html(
-//         `<div id="loader" >
-//             <img src="assets/images/photo-1501059520966-c59141726d6c.jpg" alt="loading..." />
-//          </div>`);
 
+//     $("#gh-user-data").html(
+//         `<div id="loader">
+//         <img src="assets/css/Spinner-0.9s-144px.gif" alt="Loading..." />
+//         </div>`);
+
+//     $.when(
+//         $.getJSON(`https://github.com/users/${username}`)
+//     ).then(
+//         function(response) {
+//             var userData = response ;
+//             $("#gh-user-data").html(userInformationHTML(userData))
+//         }, function(errorResponse) {
+//             if(errorResponse.status === 404) {
+//                 $("#gh-user-data").html(`<h2>No info found for user ${username} </h2>`)
+//             } else {
+//                 console.log(errorResponse);
+//                 $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+
+//             }
+//         })
 // }
+
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
 
 function fetchGitHubInformation(event) {
 
@@ -23,6 +77,26 @@ function fetchGitHubInformation(event) {
 
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/Spinner-0.9s-144px.gif"/>
+            <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
+
